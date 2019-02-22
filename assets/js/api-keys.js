622219cc0d892ccf821b8c7116abea7b4fa014c5
Sarah <sarah@sarahfrench.co.uk>
2019-02-22 15:06:15 +0000

    I give up trying to protect my API key for Giphy

--- assets/js/api-keys.js
+++
@@ -1,2 +0,0 @@
-var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
-console.log("GIPHY_API_KEY"+GIPHY_API_KEY)
--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,5 +1,5 @@
-import GIPHY_API_KEY from 'api-key';
-console.log("Hello world")
+var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
+
 //javascript, jQuery
 var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${GIPHY_API_KEY}&limit=5`);
 xhr.done(function(data) { console.log("success got data", data); });
