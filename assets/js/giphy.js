b672904826021df382fb627749d941e110856787
Sarah <sarah@sarahfrench.co.uk>
2019-02-22 14:49:52 +0000

    Trying to get API key importing ok

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,6 +1,5 @@
-const GIPHY_API_KEY = require('./api-keys.js').GIPHY_API_KEY;
-
-
+import GIPHY_API_KEY from 'api-key';
+console.log("Hello world")
 //javascript, jQuery
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${GIPHY_API_KEY}&limit=5`);
 xhr.done(function(data) { console.log("success got data", data); });
