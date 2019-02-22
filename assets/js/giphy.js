4938ed462475c82361f207c538dbe3b92a7fe434
Sarah <sarah@sarahfrench.co.uk>
2019-02-22 16:25:54 +0000

    Displays GIFs for hardcoded search for 'french' query string

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,5 +1,12 @@
 var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
 
 //javascript, jQuery
-var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${GIPHY_API_KEY}&limit=5`);
-xhr.done(function(data) { console.log("success got data", data); });
+var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=french&api_key=${GIPHY_API_KEY}&limit=5`);
+xhr.done(function(data){
+  console.log("success got data");
+  data.data.forEach( gif => {
+    let imageURL = gif.images.fixed_height.url;
+    jQuery(".giphy").append(`<div class="giphyGIF-container"><img src="${imageURL}" class="giphyGIF"></div>`)
+  })
+  // jQuery("giphy-container").append(<img src="">)
+});
