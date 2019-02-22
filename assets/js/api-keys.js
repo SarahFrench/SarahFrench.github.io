b84f5be4920d112c285b3718e59f12a2336568d3
Sarah <sarah@sarahfrench.co.uk>
2019-02-22 15:02:11 +0000

    Update api key

--- assets/js/api-keys.js
+++ assets/js/api-keys.js
@@ -1,3 +1,2 @@
-const GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
-
-export {GIPHY_API_KEY};
+var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
+console.log("GIPHY_API_KEY"+GIPHY_API_KEY)
