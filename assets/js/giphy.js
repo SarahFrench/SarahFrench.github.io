255dcc7e08e4c535894339e69e8c721e634fb502
Sarah <sarah@sarahfrench.co.uk>
2019-03-06 11:18:50 +0000

    Updating GIPHY request to use https

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,7 +1,7 @@
 var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
 
 //javascript, jQuery
-var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=french&api_key=${GIPHY_API_KEY}&limit=5`);
+var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=hackerman&api_key=${GIPHY_API_KEY}&limit=5`);
 xhr.done(function(data){
   console.log("success got data");
   data.data.forEach( gif => {
