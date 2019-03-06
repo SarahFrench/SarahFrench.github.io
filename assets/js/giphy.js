18f12c43568fdd5d07e8e25ebd7c41444060f32d
Sarah <sarah@sarahfrench.co.uk>
2019-03-06 11:26:49 +0000

    Cleaned up giphy.js file

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,6 +1,5 @@
 var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
 
-//javascript, jQuery
 var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=hackerman&api_key=${GIPHY_API_KEY}&limit=5`);
 xhr.done(function(data){
   console.log("success got data");
@@ -8,5 +7,4 @@ xhr.done(function(data){
     let imageURL = gif.images.fixed_height.url;
     jQuery(".giphy").append(`<div class="giphyGIF-container"><img src="${imageURL}" class="giphyGIF"></div>`)
   })
-  // jQuery("giphy-container").append(<img src="">)
 });
