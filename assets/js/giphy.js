745e589bc68e847412ebe0bdf195f05359e695c5
Sarah <sarah@sarahfrench.co.uk>
2019-03-14 12:45:29 +0000

    Changed $() back to $

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,6 +1,6 @@
 var GIPHY_API_KEY ='lDP1N91b3crbFDWjwtQqOHmUCN30SAnq';
 
-var xhr = $().get(`https://api.giphy.com/v1/gifs/search?q=hackerman&api_key=${GIPHY_API_KEY}&limit=5`);
+var xhr = $.get(`https://api.giphy.com/v1/gifs/search?q=hackerman&api_key=${GIPHY_API_KEY}&limit=5`);
 xhr.done(function(data){
   console.log("success got data");
   data.data.forEach( gif => {
