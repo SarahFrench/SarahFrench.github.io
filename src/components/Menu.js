ddc63fe300a9279f4dbd4b86f7a3435b23cb6db6
Sarah French <sarah@sarahfrench.co.uk>
2020-06-12 19:28:09 +0100

    Refactor menu to have set list of links, prop tells it current page But Gatsby already has some of this in the Component!

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,13 +3,32 @@ import { Link } from "gatsby"
 
 export default function Menu(props) {
 
+    const links = function(props){
+
+        let links = [
+            { href: "/", text: "Home" },
+            { href: "/about", text: "About" },
+        ];
+
+        for(let link of links){
+            if(link.text === props.currentPage){
+                link.className="current";
+            }
+        }
+
+        return links;
+    }
+
     const listItems = function(props){
-        return props.links.map( link => {
-            return (
-				<li key={link.text}>
-					<Link to={link.href}>{link.text}</Link>
-				</li>
-            );
+
+        return links(props).map(link => {
+          return (
+            <li key={link.text}>
+              <Link to={link.href} className={link.className}>
+                {link.text}
+              </Link>
+            </li>
+          )
         })
     }
 
