89932285097256b48722ea620f5509d08f9a0c04
Sarah French <sarah@sarahfrench.co.uk>
2020-06-13 01:13:33 +0100

    Menu styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby"
+import './Menu.css';
 
 export default function Menu(props) {
 
@@ -23,17 +24,16 @@ export default function Menu(props) {
 
         return links(props).map(link => {
           return (
-            <li key={link.text}>
-              <Link to={link.href} className={link.className}>
-                {link.text}
+              <Link key={link.text}
+                to={link.href}
+                className={link.className}>{link.text}
               </Link>
-            </li>
           )
         })
     }
 
-	return (<ul>
+	return (<nav className="Menu">
             {listItems(props)}
-        </ul>
+        </nav>
     );
 }
