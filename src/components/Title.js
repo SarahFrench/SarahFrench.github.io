347a0b5120a615066f38488b24be85f7958ab98b
Sarah French <sarah@sarahfrench.co.uk>
2020-06-14 21:16:01 +0100

    Replace Header component with Title, as not using that text as h1

---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+import React from "react"
+import "./Title.css"
+
+export default function Title() {
+    return <h2 className="Title">Sarah French</h2>
+}
--- src/components/header.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import "./Header.css"
-
-export default function Header(props) {
-    return <h1>{props.headerText}</h1>
-}
