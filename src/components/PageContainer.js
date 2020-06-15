660f554a500ceeab1a6cc4cc683b82d45bec18c7
Sarah French <sarah@sarahfrench.co.uk>
2020-06-15 01:09:16 +0100

    Add social media component using formantic ui

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Title from "../components/Title";
+import SocialMedia from "../components/SocialMedia"
+import "fomantic-ui-css/semantic.css"
 import './Fonts.css';
 import './PageContainer.css';
 
 function PageContainer (props){
-    return(<div className="PageContainer">
+    return (
+      <div className="PageContainer">
+        <Title />
+        <SocialMedia/>
         {props.children}
-    </div>
-    );
+      </div>
+    )
 }
 
 export default PageContainer;
